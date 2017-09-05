d24e215561f8efdb134c0e19d1f9cb747ec4549e
Tomasz Karcz <karcztomasz@hotmail.com>
2017-09-06 00:04:08 +0200

    little refactor

---
+++ WpfApp1/CheckCmd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class CheckCmd : ICommand
+    {
+        
+
+        public CheckCmd()
+        {
+            
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var vm = parameter as TabVM;
+            MessageBox.Show(vm?.FieldA + "  " + vm?.FieldB);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
--- WpfApp1/TabCtrlVM.cs
+++ WpfApp1/TabCtrlVM.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Windows;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 
 namespace WpfApp1
 {
@@ -24,33 +22,6 @@ namespace WpfApp1
                 if(_cmd != value)
                     _cmd = value;
             }
-        }
-    }
-
-    public class CheckCmd : ICommand
-    {
-        
-
-        public CheckCmd()
-        {
-            
-        }
-
-        public bool CanExecute(object parameter)
-        {
-            return true;
-        }
-
-        public void Execute(object parameter)
-        {
-            var vm = parameter as TabVM;
-            MessageBox.Show(vm?.FieldA + "  " + vm?.FieldB);
-        }
-
-        public event EventHandler CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
         }
     }
 }
